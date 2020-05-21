dbd1c67780c7ed27ec985cb5fc9f4a1c250c480d
ymzkryo <ymzk.yamachan@gmail.com>
2020-05-21 17:16:22 +0900

    add: frontend src file

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p>Home page</p>
+        <p>Random Number from backend: {{ randomNumber }}</p>
+        <button @click="getRandom">New random number</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      randomNumber: 0
+    }
+  },
+  methods: {
+    getRandom () {
+      const path = 'http://localhost:5000/api/random'
+      axios.get(path)
+        .then(response => {
+          this.randomNumber = response.data.randomNumber
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  created () {
+    this.getRandom()
+  }
+}
+</script>
